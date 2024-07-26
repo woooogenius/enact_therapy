72606169fb4f51b03c8437e09a23ead132fd88f5
woooogenius <woogenius@amuz.co.kr>
2024-07-26 17:15:02 +0900

    feat : login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,8 +1,28 @@
-
+import Input from "@enact/sandstone/Input";
+import { Panel } from "@enact/sandstone/Panels";
+import Popup from "@enact/sandstone/Popup";
+import css from '../App/App.module.less'
+import Button from "@enact/sandstone/Button";
+import Icon from "@enact/sandstone/Icon";
+import IconItem from "@enact/sandstone/IconItem";
 export default function Login() {
     return (
-        <div>
+        <Panel style={{ position: 'relative' }}>
 
-        </div>
+
+            <div className={css.login_cont}>
+
+                <h4 style={{ textAlign: 'center', letterSpacing: '8px', fontSize: '45px' }}>LOGIN</h4>
+
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Input type="text" size="small" style={{ width: '400px' }} placeholder="아이디 입력" />
+                    <IconItem style={{ background: '#1b1b1b', borderRadius: '50%' }} bordered><Icon>arrowlargeright</Icon></IconItem>
+
+                </div>
+
+            </div>
+
+
+        </Panel>
     )
 }
